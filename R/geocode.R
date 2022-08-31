55a4985b9fe7f9363a74b46cfcf97ef5f7d36b90
Cory McCartan <cory.mccartan@gmail.com>
2022-08-30 20:44:30 -0400

    testthat + scaffold

---
+++ R/geocode.R
@@ -0,0 +1,32 @@
+#' Geocode Addresses
+#'
+#' These functions efficiently geocode addresses and return either matched
+#' Census geographies (`gc_code_geo()`) or actual shapefile information
+#' (`gc_code_pt()`) with points for each address.
+#'
+#' @param addresses A data frame with parsed addresses, from [gc_address()].
+#' @param ... TBD
+#'
+#' @returns A data frame with a row for each input address. For `gc_code_geo()`,
+#'   the columns will report matched Census geographies at different levels.
+#'   For `gc_code_geo()`, there will be one column, `geometry`, with POINT
+#'   geometries for each address.
+#'
+#' @examples
+#' data(nc_addr)
+#' nc_addr |>
+#'   gc_address() |>
+#'   gc_code_geo()
+#'
+#' @name gc_code
+NULL
+
+#' @rdname gc_code
+#' @export
+gc_code_geo <- function(addresses, ...) {
+}
+
+#' @rdname gc_code
+#' @export
+gc_code_pt <- function(addresses, ...) {
+}
